2fce17c089bdbbf4607af9cedd30f44d291262d7
Nazar <sergeyko.nazar@gmail.com>
2024-08-01 12:59:53 +0300

    time api

--- api/time.js
+++ api/time.js
@@ -15,7 +15,7 @@ export default async function handler(request, response) {
       // Create the response data object
       const responseData = {
           message: "Hello World",
-          number: 192,
+          number: "192",
           time: time,
           datetime: datetime
       };
